9c6b3089fef3b5358944553d2b4e0ec5a6f645f0
John Kagga <johnkagga@gmail.com>
2017-08-20 12:46:31 +0300

    Installed Flask migration and Flask sqlalchemy

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,14 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 # Initialize application
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = ''
+
+# Initialize Flask Sql Alchemy
+db = SQLAlchemy(app)
+
+# Initialize Flask Migrate
+migrate = Migrate(app, db)
