606df9035299ced5e3620f7b1f69766b9fb6a840
John Kagga <johnkagga@gmail.com>
2017-08-20 18:42:57 +0300

    Added methods for encoding and decoding user auth tokens

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,3 +1,8 @@
 from flask import Blueprint
+from flask.views import MethodView
 
 auth = Blueprint('auth', __name__)
+
+
+class RegisterUser(MethodView):
+    pass
--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from app import app, db, bcrypt
 import datetime
+import jwt
 
 
 class User(db.Model):
@@ -17,3 +18,38 @@ class User(db.Model):
         self.password = bcrypt.generate_password_hash(password, app.config.get('BCRYPT_LOG_ROUNDS')) \
             .decode('utf-8')
         self.registered_on = datetime.datetime.now()
+
+    def encode_auth_token(self, user_id):
+        """
+        Encode the Auth token
+        :param user_id: User's Id
+        :return:
+        """
+        try:
+            payload = {
+                'exp': datetime.datetime.now() + datetime.timedelta(days=1, seconds=10),
+                'iat': datetime.datetime.now(),
+                'sub': user_id
+            }
+            return jwt.encode(
+                payload,
+                app.config['SECRET_KEY'],
+                algorithm='HS256'
+            )
+        except Exception as e:
+            return e
+
+    @staticmethod
+    def decode_auth_token(token):
+        """
+        Decoding the token to get the payload and then return the user Id in 'sub'
+        :param token: Auth Token
+        :return:
+        """
+        try:
+            payload = jwt.decode(token, app.config['SECRET_KEY'])
+            return payload['sub']
+        except jwt.ExpiredSignatureError:
+            return 'Signature expired, Please sign in again'
+        except jwt.InvalidTokenError:
+            return 'Invalid token. Please sign in again'
