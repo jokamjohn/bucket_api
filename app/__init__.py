d3a597215831da68acceb5cb20b5d3029cb4f432
johnkagga <johnkagga@gmail.com>
2017-10-07 18:28:32 +0300

    Enabled CORS

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,13 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_cors import CORS
 
 # Initialize application
 app = Flask(__name__, static_folder=None)
+
+# Enabling cors
+CORS(app)
 
 # app configuration
 app_settings = os.getenv(
