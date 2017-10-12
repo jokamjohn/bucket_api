6872961190287390121ae566c04777bf6834b006
johnkagga <johnkagga@gmail.com>
2017-10-12 22:43:51 +0300

    fixing cors on bucketlist route

--- app/bucket/views.py
+++ app/bucket/views.py
@@ -3,7 +3,6 @@ from app.auth.helper import token_required
 from app.bucket.helper import response, response_for_created_bucket, response_for_user_bucket, response_with_pagination, \
     get_user_bucket_json_list, paginate_buckets
 from app.models import User, Bucket
-from flask_cors import cross_origin
 
 # Initialize blueprint
 bucket = Blueprint('bucket', __name__)
@@ -29,8 +28,7 @@ def bucketlist(current_user):
     return response_with_pagination([], previous, nex, 0)
 
 
-@bucket.route('/bucketlists', methods=['POST'])
-@cross_origin()
+@bucket.route('/bucketlists/', methods=['POST'])
 @token_required
 def create_bucketlist(current_user):
     """
